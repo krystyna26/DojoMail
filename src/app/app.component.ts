2de05f2f751890f88d73d365c2e755632eb1d7d0
Krystyna Swider <krystynaswider@admins-MacBook-Pro-2.local>
2017-12-01 11:41:05 -0800

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Dojo Mail'
+  
+  emails = [
+    {email:'gfds@gmail.com', important: true, mySubjects:"New windows" ,myContent: "Enchantress of numbers"},
+    {email:'arq@yahoo.com', important:true ,mySubjects:" Programming" ,myContent:"New algorithm for shadow volumes"},
+    {email: 'krysia@gamil.com', important:false ,mySubjects:" Uploaded Algo" ,myContent:"Just kidding"},
+    {email:'asd@gmail.com', important: false,mySubjects:"Windows IX will launch in 2100" ,myContent:"Tralalala"}
+ 
+  ]
 }
